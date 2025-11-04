f069e39a42ccca80094a6e14380e97e108c0a86b
Ulises Jaramillo <ulises.jp@outlook.com>
2025-11-04 17:26:24 -0600

    fix: mail service import and requirements

--- backend/app/utils/tests/mail_send_test.py
+++ backend/app/utils/tests/mail_send_test.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from mail import send_email
+from mail.mail import send_email
 
 #  test
 send_email("example@gmail.com", "Test", "<h1>✅ SMTP works!</h1>")
